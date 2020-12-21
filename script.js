426cb8ff63e6db42636de78b20c9142de16b4bfe
HoustonOla35 <zzeroexplorer@gmail.com>
2020-12-21 21:25:28 +0100

    Change slider

--- script.js
+++ script.js
@@ -1,21 +1,14 @@
-function startSliders() {
-  document.querySelectorAll('.slider').forEach((slider, i) => {
-    M.Slider.getInstance(slider).start();
-  });
-}
-
-function pauseSliders() {
-  document.querySelectorAll('.slider').forEach((slider, i) => {
-    M.Slider.getInstance(slider).pause();
-  });
-}
-
 // Initialisation
 document.addEventListener('DOMContentLoaded', function() {
-  M.Slider.init(document.querySelectorAll('.slider'), {height:560});
 
-  M.Materialbox.init(document.querySelectorAll('.materialboxed'), {
-    onOpenStart: pauseSliders,
-    onCloseEnd: startSliders
-  });
+  // Sliders
+  var sliders = document.getElementsByClassName('splide');
+  for (var i=0; i<sliders.length; i++) {
+  	new Splide(sliders[i], {
+      autoplay: true,
+      speed: 600,
+      rewind: true
+    }).mount();
+  }
+
 });
