50775ef8d424ca22204ea63efd87f471686e78d3
HoustonOla35 <zzeroexplorer@gmail.com>
2021-04-25 13:45:00 +0200

    Consommation

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 // Initialisation
 document.addEventListener('DOMContentLoaded', function() {
 
-  // Sliders
-  var sliders = document.getElementsByClassName('splide');
-  for (var i=0; i<sliders.length; i++) {
-  	new Splide(sliders[i], {
-      autoplay: true,
-      speed: 600,
-      rewind: true
-    }).mount();
-  }
+    // Sliders
+    var sliders = document.getElementsByClassName('splide');
+    for (var i=0; i<sliders.length; i++) {
+        new Splide(sliders[i], {
+            autoplay: true,
+            speed: 600,
+            rewind: true
+        }).mount();
+    }
 
-  // Tabs
-  var tabs = document.getElementsByClassName('tabs');
-  for (var i=0; i<tabs.length; i++) {
-    M.Tabs.init(tabs[i], {});
-  }
+    // Tabs
+    var tabs = document.getElementsByClassName('tabs');
+    for (var i=0; i<tabs.length; i++) {
+        M.Tabs.init(tabs[i], {});
+    }
 
+    // Modals
+    M.Modal.init(document.querySelectorAll('.modal'), {});
 });
